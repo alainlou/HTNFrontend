798f6ae66c6c1951315dac9c4ae842f435bae12c
alainlou <az2lou@uwaterloo.ca>
2019-09-15 07:33:03 -0400

    first deploy to gh pages

---
+++ precache-manifest.9ae71de979849fff5fd9421d29a8dc56.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba98d4de77448aad8d42eab1a98fbf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf9eeb6b3fbd9d88989",
+    "url": "/static/css/2.eec0e7d2.chunk.css"
+  },
+  {
+    "revision": "47740673607a2baf2f26",
+    "url": "/static/css/main.1110ed02.chunk.css"
+  },
+  {
+    "revision": "baf9eeb6b3fbd9d88989",
+    "url": "/static/js/2.d910bfc2.chunk.js"
+  },
+  {
+    "revision": "47740673607a2baf2f26",
+    "url": "/static/js/main.0460af13.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  },
+  {
+    "revision": "32043a84925ad4ed0a959b45ae3c8da4",
+    "url": "/static/media/bennet.32043a84.jpg"
+  },
+  {
+    "revision": "07e03acadd7dfb5c7094200bfab86d26",
+    "url": "/static/media/bernie.07e03aca.jpg"
+  },
+  {
+    "revision": "c3fe8b843330850a4c1e3b0f45b72cba",
+    "url": "/static/media/biden.c3fe8b84.png"
+  },
+  {
+    "revision": "85a1c38152dbff654aaa3db4ca03ae69",
+    "url": "/static/media/blue.85a1c381.png"
+  },
+  {
+    "revision": "ed1bf4faca3e9c7a3177779e904d1a98",
+    "url": "/static/media/booker.ed1bf4fa.jpg"
+  },
+  {
+    "revision": "5e5417627a7ecd9d1ec9054e6061bf67",
+    "url": "/static/media/buttgieg.5e541762.jpeg"
+  },
+  {
+    "revision": "7eb5aca9043fed2ce41b6d1f3a07c785",
+    "url": "/static/media/donaldtrump.7eb5aca9.jpg"
+  },
+  {
+    "revision": "e6f97f75399a7d347534b7b4b84979e5",
+    "url": "/static/media/hillary.e6f97f75.jpg"
+  },
+  {
+    "revision": "cdc97cdc09e39d2ca1d674eb2c7e99a1",
+    "url": "/static/media/melee.cdc97cdc.mp3"
+  },
+  {
+    "revision": "cfab92a3925a87b27820181eac72d252",
+    "url": "/static/media/red.cfab92a3.png"
+  },
+  {
+    "revision": "7188f4b7fd2dd334d2f9c559facb5ff8",
+    "url": "/static/media/sanders.7188f4b7.jpg"
+  }
+]);

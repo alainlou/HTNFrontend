e7aa0ecbbd34e93f3666819aa02dd86af8227e9c
Kevin He <yc4he@uwaterloo.ca>
2019-09-15 00:16:37 -0400

    added axios

--- src/App.js
+++ src/App.js
@@ -22,30 +22,23 @@ class App extends React.Component {
     this.onSubmit = this.onSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-  // getNewState = async () => {
-  //   try {
-  //     return Axios.get("https://htnbackend.appspot.com/", {params:{searchWord: this.state.searchWord}});
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+  
   handleChange(event){
     this.setState({searchWord: event.target.value.toLowerCase() || ""});
   }
+
   onSubmit(event){
     Axios.get("https://htnbackend.appspot.com/", {params:{searchWord: this.state.searchWord}})
       .then(response => {
         this.setState({videoUrl: response.data});
       });
-    // console.log(newData);
-    // this.setState({data: newData});
     event.preventDefault();
   }
   render(){
     return (
       <div className="container-fluid">
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Video Indexer</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
@@ -60,7 +53,7 @@ class App extends React.Component {
             </Navbar.Collapse>
         </Navbar>   
         <div className="grey-background">
-          <ReactPlayer className="middle-align" url={this.state.videoUrl} playing />
+          <ReactPlayer className="middle-align" url={this.state.videoUrl} playing controls/>
         </div>
         <div className="flex-container my-3" id="main_section">
           <span className="col-12">
